a21cd3b2c4bdc12cd62486caa429674b3f229102
jared b <jaredcbaum@gmail.com>
2020-08-01 15:27:54 -0500

    saturday afternoon

--- src/pages/landing-page/components/appflow-activatior/appflow-activator.tsx
+++ src/pages/landing-page/components/appflow-activatior/appflow-activator.tsx
@@ -1,6 +1,6 @@
 import { Component, State, Listen, Element, Host, h, getAssetPath, Watch, Prop } from '@stencil/core';
-import { publishIcon, updatesIcon, buildsIcon, automationsIcon } from './activator-icons'
-import { ResponsiveContainer, Heading, Paragraph } from '@ionic-internal/ionic-ds';
+import { publishIcon, updatesIcon, buildsIcon, automationsIcon } from './assets/icons'
+import { ResponsiveContainer, Heading, Paragraph, Skeleton } from '@ionic-internal/ionic-ds';
 
 
 @Component({
@@ -17,34 +17,30 @@ export class AppflowActivator {
       name: 'App Publishing',
       description: 'Publish directly to the Apple and Google App Stores.',
       icon: publishIcon,
-      image: getAssetPath('./assets/screen-app-publishing.png')
+      image: getAssetPath('./assets/img/screen-app-publishing.png')
     },
     {
       name: 'Live Updates',
       description: 'Send live updates to users without waiting on app store approval.',
       icon: updatesIcon,
-      image: getAssetPath('./assets/screen-live-updates.png')
+      image: getAssetPath('./assets/img/screen-live-updates.png')
     },
     {
       name: 'Native Builds',
       description: 'Compile native app binaries in the cloud.',
       icon: buildsIcon,
-      image: getAssetPath('./assets/screen-native-builds.png')
+      image: getAssetPath('./assets/img/screen-native-builds.png')
     },
     {
       name: 'Automations',
       description: 'Fully automate your app delivery pipeline.',
       icon: automationsIcon,
-      image: getAssetPath('./assets/screen-automations.png')
+      image: getAssetPath('./assets/img/screen-automations.png')
     },
   ];
 
   @Element() el: HTMLElement;
 
-  @State() imageList: { maxHeight: number, images: HTMLElement[] } = {
-    maxHeight: 0,
-    images: []
-  };
   @State() currentScreen = 0;
   @State() isPaused: boolean = false;
 
@@ -111,14 +107,7 @@ export class AppflowActivator {
 
 
   handleImageLoad(el: HTMLElement) {
-    this.imageList.images.push(el);
 
-    if (el.offsetHeight > this.imageList.maxHeight) {
-      this.imageList = {
-        maxHeight: el.offsetHeight,
-        images: [...this.imageList.images]
-      }
-    }
   }
 
   @Listen('scroll', {target: 'window'})
@@ -137,41 +126,37 @@ export class AppflowActivator {
     }
   }
 
-  @Listen('resize', { target: 'window'})
-  updateSize() {
-    this.imageList = {
-      maxHeight: this.imageList.images[0].offsetHeight,
-      images: [...this.imageList.images]
-    }
-  }
-
   render() {
     return (
     <Host>
       <div class="app-screenshot">
-        <div class="wrapper" style={{ 'height': this.imageList.maxHeight + 'px' }}>
-          {this.screens.map((screen, i) =>
-            <div class={`screen ${i === this.currentScreen ? 'animate-in' : 'animate-out'}`}>
-              <img src={screen.image} onLoad={({target}) => this.handleImageLoad(target as HTMLElement)}/>
-            </div>
-          )}
+        {this.screens.map((screen, i) => (
+          <img
+            class={`screen ${i === this.currentScreen ? 'animate-in' : 'animate-out'}`}
+            src={screen.image}
+            width="1153"
+            height="611"
+            loading={i === 0 ? 'eager' : 'lazy'}
+            onLoad={({target}) => this.handleImageLoad(target as HTMLElement)}
+            style={{'position': i !== 0 ? 'absolute' : undefined}}
+          />
+        ))}
+        <div class="nav">
+          <ResponsiveContainer>
+            <ul>
+              {this.screens.map((screen, i) =>
+                <li
+                  class={(i === this.currentScreen) ? 'active' : 'default'}
+                  onMouseEnter={() => this.override(i)}>
+                  {screen.icon(i === this.currentScreen ? 'active' : 'default')}
+                  <Heading level={5}>{screen.name}</Heading>
+                  <Paragraph level={4}>{screen.description}</Paragraph>
+                  <div class="indicator" ref={(el) => this.indicators[i] = el}></div>
+                </li>
+              )}
+            </ul>
+          </ResponsiveContainer>
         </div>
-      </div>
-      <div class="nav">
-        <ResponsiveContainer>
-          <ul>
-            {this.screens.map((screen, i) =>
-              <li
-                class={(i === this.currentScreen) ? 'active' : 'default'}
-                onMouseEnter={() => this.override(i)}>
-                {screen.icon(i === this.currentScreen ? 'active' : 'default')}
-                <Heading level={5}>{screen.name}</Heading>
-                <Paragraph level={4}>{screen.description}</Paragraph>
-                <div class="indicator" ref={(el) => this.indicators[i] = el}></div>
-              </li>
-            )}
-          </ul>
-        </ResponsiveContainer>
       </div>
     </Host>
     );
